d52239bf05344b99cbed634ea0f40d4204d6627d
andrey dolgashov <adolgashov91@gmaol.com>
2024-09-01 22:37:38 +0300

    change package.json, index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'node:fs';
 import path from 'node:path';
 
-const getPath = (filepath) => path.resolve(process.cwd(), filepath)
+const getPath = (filepath) => path.resolve(filepath)
 
 const parser = (path) => JSON.parse(path)
 
@@ -11,11 +11,11 @@ const gendiff = (filePath1, filePath2) => {
   const file1 = readFileSync(getPath(filePath1))
   const file2 = readFileSync(getPath(filePath2))
 
-  const data1 = parser(file1)
-  const data2 = parser(file2)
+  const data1 = parser(file1);
+  const data2 = parser(file2);
 
   console.log(data1);
   console.log(data2);
 
-}
+};
 export default gendiff;
