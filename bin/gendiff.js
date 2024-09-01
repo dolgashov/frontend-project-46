ed14dffa14799d04c385d1caf86e79d10a5d9b89
andrey dolgashov <adolgashov91@gmaol.com>
2024-09-01 22:29:43 +0300

    add fixtures, src, index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-
+import gendiff from '../src/index.js';
 const program = new Command();
 
 program
@@ -8,5 +8,7 @@ program
   .description('Compares two configuration files and show a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filePath1> <filePath2>')
-
+  .action((filePath1, filePath2) => {
+    gendiff(filePath1, filePath2)
+  })
   program.parse(process.argv);
