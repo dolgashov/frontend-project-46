d59a8d0cb1cfef575dc3540e820807f985a31950
andrey dolgashov <adolgashov91@gmaol.com>
2024-09-01 16:50:48 +0300

    add option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,5 @@ const program = new Command();
 program
   .version('0.1.0')
   .description('Compares two configuration files and show a difference.')
-  .parse(process.argv);
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filePath1> <filePath2>')
